f08a0cc6857bf77097ff4ee4c1dd7d64edd822ed
ITSAIDI <noureddinesaidi111@gmail.com>
2025-05-19 17:06:00 +0200

    c

---
+++ sauvegarde/scripts/commands.sql
@@ -0,0 +1,10 @@
+--select * from chaines ;
+--select * from videos;
+SELECT * FROM videos WHERE id_chaine = 'UCVQeGg4Fdrrr8vDXa7yjOYg';
+--select * from chaines where nom = 'IADEA';
+--SELECT * FROM videos WHERE id_video = 'PaBTFTR_BaA';
+--select * from chaines where pertinente = true;
+
+
+
+
