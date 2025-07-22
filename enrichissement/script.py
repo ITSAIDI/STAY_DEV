31a8db2424f258c2eb0dd3487f35bb6f038433ff
ITSAIDI <noureddinesaidi111@gmail.com>
2025-07-22 18:01:51 +0200

    update

--- enrichissement/script.py
+++ enrichissement/script.py
@@ -1,22 +1,24 @@
 import psycopg
 import json
 import os
+import sys
+from tqdm import tqdm
 from dotenv import load_dotenv
 load_dotenv()
-
 
 def openJson(path):
     with open(path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
-jsonfile = './jsons/output.json'
+# Global variables
+
+jsonfile = sys.argv[1]
 data = openJson(jsonfile)
-print(len(data))
 
 # Connect to youtubstay
 
-"""conn = psycopg.connect(
+conn = psycopg.connect(
     dbname="youtubestay",
     user="postgres",
     password=os.getenv("POSTGRE_PASSWORD"),
@@ -25,28 +27,58 @@ print(len(data))
 )
 
 
-cur = conn.cursor()"""
+cur = conn.cursor()
 
-import folium
+def labelExist(label):
+    cur.execute("""
+      SELECT label FROM entites_spatiales; 
+        """)
+    rows = cur.fetchall()
+    for row in rows:
+        if row[0]==label:
+            return True
+    return False
 
-location_data = {
-        "lat": 49.5532646,
-        "lon": 2.9392577,
-        "ent": "ville"
-      }
+def generateId():
+    cur.execute("""
+      SELECT count(*) FROM entites_spatiales; 
+        """)
+    rows = cur.fetchall()
+    count = rows[0][0]
+    return f'es{count+1}'
+    
+def getEntityID(label):
+    if labelExist(label):
+        cur.execute("SELECT id_entite_spatiale FROM entites_spatiales WHERE label = %s;", (label,))
+        rows = cur.fetchall()
+        return rows[0][0]
+    return generateId()
 
-map_obj = folium.Map(location=[location_data["lat"], location_data["lon"]], zoom_start=13)
+def updateTables():
+    for video in tqdm(data):
+        if 'output' in video:
+            for entity in video['output']:
+                entId = getEntityID(entity['ent'])
+            
+                if labelExist(entity['ent'])==False:
+                    cur.execute("insert into entites_spatiales values (%s, %s, %s, %s)",(entId, entity['ent'], entity['lat'], entity['lon']))
+                    
+                cur.execute("insert into entites_spatiales_videos values (%s, %s) ON CONFLICT DO NOTHING",(entId, video['id_video']))
+    
+                     
+#print(labelExist('paris'))
+#print(generateId())
+#print(getEntityID('paris'))
 
-folium.Marker(
-    [location_data["lat"], location_data["lon"]],
-    popup=location_data["ent"],
-    tooltip=location_data["ent"]
-).add_to(map_obj)
 
-map_obj.save("map_janze.html")
+updateTables()
+
+#print(len(data))
 
 
-
+conn.commit()
+cur.close()
+conn.close()
 
 
 
